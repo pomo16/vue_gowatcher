73b030cb2dc6be77cf50a437304b995386c8fb9b
pomo <m13143378602@163.com>
2020-03-18 17:21:56 +0800

    add navigation

---
+++ src/components/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-container class="home-container">
+    <!-- header -->
+    <el-header>
+      <div>
+        <img src="../assets/gowatcher.png" alt="">
+        <span>gowatcher</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- body -->
+    <el-container>
+      <!-- body left -->
+      <el-aside width="200px">
+        <!-- menu -->
+        <el-menu
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <!-- 1st menu -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <!-- 2nd menu -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- body right -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
+  .el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+      display: flex;
+      align-items: center;
+      span {
+        margin-left: 15px;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// import font and icon
+import './assets/fonts/iconfont.css'
+// import global css
+import './assets/css/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
